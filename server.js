c1c4b99a0bdcc57228da8883d640c924d252c7c7
Nikolay Sokolov <117732387+dlrxgit@users.noreply.github.com>
2023-06-23 19:10:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000, () => {
-  console.log('App started on port 3000');
+  console.log('Server listening on port 3000.');
 });
 
 function getFileContents(filename) {
